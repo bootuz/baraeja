26e15caa732cabb715aec2b4c1c4ea6f85f47c74
Astemir Boziev <bootuz07@gmail.com>
2023-03-29 22:52:32 +0400

    changed commands

--- baraeja/settings.py
+++ baraeja/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = 'static/'
 if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
 
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
