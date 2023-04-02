892b27b65831d6f630bfe460a5ac8d9f7388b110
Astemir Boziev <bootuz07@gmail.com>
2023-04-02 13:01:35 +0400

    added baraeja.com domain in ALLOWED_HOSTS

--- baraeja/settings.py
+++ baraeja/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='local_secret')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['baraeja.com']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
