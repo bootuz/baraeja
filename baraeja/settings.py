ff8c53bfa8e17dd36bfbd8ef479646d652b88a49
Astemir Boziev <bootuz07@gmail.com>
2023-04-02 21:22:19 +0400

    added staging database

--- baraeja/settings.py
+++ baraeja/settings.py
@@ -99,7 +99,7 @@ else:
             "TEST": {"NAME": "test.sqlite3"},
         }
     }
-
+print(DATABASES["default"])
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
