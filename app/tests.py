53341fd15449b78ea555bbf99232e697fba5382c
Astemir Boziev <17073255+bootuz@users.noreply.github.com>
2023-04-13 12:29:35 +0400

    New tests (#44)
    * added new tests for Category model
    
    * small fix

--- app/tests.py
+++ app/tests.py
@@ -13,13 +13,36 @@ def generate_random_string(length: int):
 
 class Test(TestCase):
     def test_category(self):
-        title = generate_random_string(10)
-        slug = generate_random_string(10)
+        length = random.randint(10, 99)
+        title = generate_random_string(length)
+        slug = generate_random_string(length)
         category = Category.objects.create(title=title, slug=slug)
         assert category.title == title
         assert category.slug == slug
+        assert len(category.title) == length
+        assert len(category.slug) == length
+
+    def test_category_no_slug(self):
+        length = random.randint(10, 99)
+        title = generate_random_string(length)
+        category = Category.objects.create(title=title)
+        assert category.title == title
+        assert category.slug == ""
+        assert len(category.title) == length
+        assert len(category.slug) == 0
+
+    def test_category_no_title(self):
+        length = random.randint(10, 99)
+        slug = generate_random_string(length)
+        category = Category.objects.create(slug=slug)
+        assert category.slug == slug
+        assert category.title == ""
+        assert len(category.slug) == length
+        assert len(category.title) == 0
 
     def test_category_str(self):
-        title = generate_random_string(10)
+        length = random.randint(10, 99)
+        title = generate_random_string(length)
         category = Category.objects.create(title=title, slug=title)
         assert str(category) == title
+        assert len(category.title) == length
