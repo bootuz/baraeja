c0f926751f57277fed231709a6a68b6ed9f4ca73
Astemir Boziev <bootuz07@gmail.com>
2023-05-10 23:09:25 +0400

    make all fields required

--- app/forms.py
+++ app/forms.py
@@ -7,18 +7,15 @@ class ContactForm(forms.Form):
         widget=forms.TextInput(
             attrs={"class": "form-control feedback", "placeholder": "Уи цӏэр"}
         ),
-        required=False,
     )
     email = forms.CharField(
         widget=forms.EmailInput(
             attrs={"class": "form-control feedback", "placeholder": "Уи email-р"}
         ),
-        required=False,
     )
     message = forms.CharField(
         max_length=500,
         widget=forms.Textarea(
             attrs={"class": "form-control feedback", "placeholder": "Тхыгъэр"}
         ),
-        required=False,
     )
