0b336f629cdd4f360f126a6db0765557f06ad46b
Astemir Boziev <bootuz07@gmail.com>
2023-05-10 22:12:52 +0400

    added contact form class

--- app/forms.py
+++ app/forms.py
@@ -3,10 +3,22 @@ from django import forms
 
 class ContactForm(forms.Form):
     name = forms.CharField(
-        max_length=255, widget=forms.TextInput(attrs={"class": "form-control"})
+        max_length=255,
+        widget=forms.TextInput(
+            attrs={"class": "form-control feedback", "placeholder": "Уи цӏэр"}
+        ),
+        required=False,
     )
-    email = forms.CharField(widget=forms.EmailInput(attrs={"class": "form-control"}))
+    email = forms.CharField(
+        widget=forms.EmailInput(
+            attrs={"class": "form-control feedback", "placeholder": "Уи email-р"}
+        ),
+        required=False,
+    )
     message = forms.CharField(
         max_length=500,
-        widget=forms.Textarea(attrs={"class": "form-control", "rows": "7"}),
+        widget=forms.Textarea(
+            attrs={"class": "form-control feedback", "placeholder": "Тхыгъэр"}
+        ),
+        required=False,
     )
